45483d1e5197e3e5a538e594afc4ca81656b6c87
bebetterthinker <www.thinkbetterthanyouguess@gmail.com>
2024-05-16 12:07:32 +0300

    add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBage from "@/app/components/IssueStatusBage";
 import prisma from "@/prisma/client";
 import { Status } from "@prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 interface Props {
   params: { id: string };
 }
@@ -14,21 +16,29 @@ const IssueDetailPage = async ({ params }: Props) => {
   });
 
   return (
-    <div>
-      {issue && (
-        <>
-          <Heading>{issue.title}</Heading>
-          <Flex className="space-x-3" my="2">
-            <IssueStatusBage status={issue.status as Status} />
-            <p>{issue.createdAt.toDateString()}</p>
-          </Flex>
-          <Card className="prose">
-            <ReactMarkdown>{issue.descriptions as string}</ReactMarkdown>
-          </Card>
-        </>
-      )}
-      {!issue && <p>Issue not found.</p>}
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} className="mx-w-xl" gap="5">
+      <Box>
+        {issue && (
+          <>
+            <Heading>{issue.title}</Heading>
+            <Flex className="space-x-3" my="2">
+              <IssueStatusBage status={issue.status as Status} />
+              <p>{issue.createdAt.toDateString()}</p>
+            </Flex>
+            <Card className="prose">
+              <ReactMarkdown>{issue.descriptions as string}</ReactMarkdown>
+            </Card>
+          </>
+        )}
+        {!issue && <p>Issue not found.</p>}
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue?.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@radix-ui/themes";
 import { Skeleton } from "@/app/components";
-
 const NewIssueLOadingPage = () => {
   return (
     <Box className="max-w-xl">
